e0fe20ddbe0dc39c2e3cfbe8043d7afbcc6561bd
Rishabh Singh <73096439+17rishu@users.noreply.github.com>
2024-09-04 20:18:07 +0000

    datatypes done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";   //treat all JS code as per the latest version
+
+// alert(3 + 66)    // we r using nodeJs, not browser
+
+console.log(3)
+
+console.log("Hello")
+
+let name = "Rishabh"
+let age = 21
+let isLoggedIn = false
+let state
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => not defined
+// symboles => unique
+
+// object
+
+console.log(typeof undefined)   // undefined
+console.log(typeof null)   // object
