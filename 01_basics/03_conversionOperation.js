27d739485b0faab2e57a45ae51aad535a7b6fada
Rishabh Singh <73096439+17rishu@users.noreply.github.com>
2024-09-05 11:05:33 +0000

    study about confusing conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -17,3 +17,30 @@ console.log(valueInMixed)   // NaN, which means Not a Number and mixed can not b
 // false/true => 0/1
 // undefined => undefined
 // null => NaN
+
+let str1 = "hello "
+let str2 = "rishabh"
+
+let str3 = str1 + str2;
+
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true)  //1
+console.log(+"")    //0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2*2    //all will be assigned by 2*2
+
+let gameCounter = 100
+
+gameCounter++
+
+console.log(gameCounter)
+console.log(gameCounter++)
+console.log(gameCounter)
