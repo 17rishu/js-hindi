4d29900e224cc0dc3a04333a948ced4347ac115b
Rishabh Singh <73096439+17rishu@users.noreply.github.com>
2024-09-10 23:14:49 +0000

    Conditional Operations

---
+++ 04_control_flow/one.js
@@ -0,0 +1,57 @@
+const temperature = 25
+
+// if(temperature < 30){
+//     console.log(`Temperature is less than 30`);   
+// }
+
+// else{
+//     console.log(`Temperature is more than 30`);
+// }
+
+const score = 200
+
+// if(score > 100){
+//     const power = "fly"
+//     console.log(`Unlocked Power is ${power}`);
+// }    
+
+// console.log(`Unlocked Power is ${power}`);
+
+
+const useremail = "r@rishabh.ai"
+
+// if(useremail)
+//         console.log("Got the email");
+
+// else
+//     console.log("No email");
+
+
+// falsy values
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy value
+// "0", 'false', " ", [], {}, function(){}
+
+
+const array = []
+
+// if(array.length === 0)
+//     console.log("Empty Array");
+
+const obj = {}
+
+// if(Object.keys(obj).length === 0)
+//     console.log("Empty Object")
+
+
+// Null Coalescing Operator (??) : null undefined
+
+let val1
+
+// val1 = 5 ?? 10
+// val1 = null ?? 15
+// val1 = undefined ?? 20
+// val1 = null ?? 10 ?? 20 ?? 30      // it will assign the very first true value
+
+// console.log(val1)
