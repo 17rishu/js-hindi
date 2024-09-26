54deeb16a4348b126521de57b4098e0179798e3c
Rishabh Singh <73096439+17rishu@users.noreply.github.com>
2024-09-26 09:37:09 +0000

    getOwnPropertyDescriptor understanding

---
+++ 10_classes_and_oops/mathPI.js
@@ -0,0 +1,33 @@
+// console.log(Math.PI)
+// Math.PI = 5
+// console.log(Math.PI)
+
+const descriptor = Object.getOwnPropertyDescriptor(Math, 'PI')
+
+// console.log(descriptor)
+
+const chai = {
+    name: 'gingerChai',
+    price: 250,
+    isAvailable: true,
+
+    order: function(){
+        console.log('chai ni bn paegi!')
+    }
+}
+
+// console.log(chai)
+
+console.log(Object.getOwnPropertyDescriptor(chai, 'name'))
+
+Object.defineProperty(chai, 'name', {
+    // writable: false,
+    enumerable: false
+})
+
+console.log(Object.getOwnPropertyDescriptor(chai, 'name'))
+
+for (const [key, value] of Object.entries(chai)) {
+    if(typeof value !== 'function')
+        console.log(`${key} -> ${value}`)
+}
